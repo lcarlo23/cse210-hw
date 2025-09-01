a0ab16f1658f380d7fa3a4e1df81e45e7acd37ef
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-09-01 08:55:13 +0200

    Completed exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess;
+
+        do
+        {
+            Console.WriteLine("What is the your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
+
+
     }
 }
