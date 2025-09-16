bc7d79c34ecfe7f6c5c14004c108625076cbcf86
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-09-16 17:57:41 +0200

    Completed Encapsulation Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction fraction2 = new Fraction(1);
+        Fraction fraction3 = new Fraction(5);
+        Fraction fraction4 = new Fraction(3, 4);
+        Fraction fraction5 = new Fraction(1, 3);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetTop());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetTop());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetTop());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
     }
 }
