d54beb3efbb7a1610436c2f3403a7694f652260f
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-10-06 07:20:00 +0200

    Completed Polymorphism Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("white", 25);
+        Rectangle rectangle = new Rectangle("red", 25, 15);
+        Circle circle = new Circle("green", 14);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine();
+            Console.WriteLine($"The color is: {color}.");
+            Console.WriteLine($"The area is {area}.");
+        }
     }
 }
